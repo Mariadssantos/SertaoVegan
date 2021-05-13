45f87b129c0572a0e566e5801bc7f463f59f8859
Maria Eduarda  Santos <58342658+Mariadssantos@users.noreply.github.com>
2021-05-13 20:34:08 -0300

    Contéudo dos cards

--- src/pages/Receitas/[slug].tsx
+++ src/pages/Receitas/[slug].tsx
@@ -1,10 +1,14 @@
 import { GetStaticPaths, GetStaticProps } from "next";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import Head from "next/head";
-import Link from "next/link";
 
+import ContentFooter from "../../Components/Footer";
+import ContentHeader from "../../Components/Header";
+
+// import Link from "next/link";
 import { api } from "../../../Services/api";
 
+import styles from "./content.module.scss";
 
 type Receita = {
   id: string;
@@ -13,7 +17,24 @@ type Receita = {
   image: string;
   porcoes: number;
   descriptioningredientes: {
-      ingrediente1: string;
+    ingrediente1: string;
+    ingrediente2: string;
+    ingrediente3: string;
+    ingrediente4: string;
+    ingrediente5: string;
+    ingrediente6: string;
+    ingrediente7: string;
+    ingrediente8: string;
+    ingrediente9: string;
+    ingrediente10: string;
+    ingrediente11: string;
+    ingrediente12: string;
+    ingrediente13: string;
+    ingrediente14: string;
+    ingrediente15: string;
+    ingrediente16: string;
+    ingrediente17: string;
+    ingrediente18: string;
   };
   descriptionpreparo: string;
 };
@@ -21,21 +42,62 @@ type ReceitaProps = {
   receita: Receita;
 };
 
-
 export default function Receita({ receita }: ReceitaProps) {
-
-  <Head>
-    <title> {receita.title} |  </title>
-  </Head>;
+  <Head>{/* <title> SV | {receita.title} </title> */}</Head>;
   // é da parte de cima no caso da abinha
 
   return (
     <div>
-        <h1>{receita.title}</h1>
-        <h2>{receita.descriptioningredientes.ingrediente1}</h2>
+      <ContentHeader />
 
+      <div className={styles.title}>
+        <h2>{receita.title}</h2>
+      </div>
+
+      <div className={styles.detalhes}>
+        <div>
+          <h3>Tempo de Preparo: {receita.times}</h3>
+        </div>
+        <div>
+          <h3>Porções: {receita.porcoes}</h3>
+        </div>
+      </div>
+
+      <div className={styles.containerContent}>
+        <div className={styles.image}>
+          <img src={receita.image} alt="Imagem" />
+        </div>
+        <div className={styles.ContentEnd}>
+          <div className={styles.ingredientes}>
+            <h3>Ingredientes</h3>
+            <h4>{receita.descriptioningredientes.ingrediente1}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente2}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente3}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente4}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente5}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente6}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente7}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente8}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente9}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente10}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente11}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente12}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente13}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente14}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente15}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente16}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente17}</h4>
+            <h4>{receita.descriptioningredientes.ingrediente18}</h4>
+          </div>
+
+          <div className={styles.preparo}>
+            <h3>Modo de Preparo</h3>
+            <h4>{receita.descriptionpreparo}</h4>
+          </div>
+        </div>
+      </div>
+      <ContentFooter />
     </div>
-    
   );
 }
 
@@ -70,10 +132,9 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
     times: data.times,
     image: data.image,
     porcoes: data.porcoes,
-    descriptioningredientes:data.descriptioningredientes,
-    descriptionpreparo: data.descriptionpreparo
-  }
-    
+    descriptioningredientes: data.descriptioningredientes,
+    descriptionpreparo: data.descriptionpreparo,
+  };
 
   return {
     props: {
